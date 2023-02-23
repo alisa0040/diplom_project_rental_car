1c6d482deeca8a29da45e9211cd0ed61f2fd7364
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-02-23 20:24:11 +0200

    Delete reservation admin.py

--- reservation admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from reservation.models import Reservation
-
-
-@admin.register(Reservation)
-class ReservationAdmin(admin.ModelAdmin):
-    list_display = ["customer", "car","start_date","end_date"]
