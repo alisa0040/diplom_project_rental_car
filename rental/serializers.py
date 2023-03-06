1cbd3e44ee22d7a5c02b541eb1c0a7a46ba997a2
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-03-06 08:53:04 +0200

    Update serializers.py

--- rental/serializers.py
+++ rental/serializers.py
@@ -1,8 +1,9 @@
 from rental.models import Rental
 from rest_framework import serializers
+from car.serializers import CarSerializer
 
 class RentalSerializer(serializers.ModelSerializer):
-    total_amount = serializers.ReadOnlyField()
+    total_amount = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Rental
