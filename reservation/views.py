78cdbbd0959268479247292fd5fffed6a59bfd33
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-03-06 09:46:26 +0200

    Update views.py

--- reservation/views.py
+++ reservation/views.py
@@ -9,10 +9,14 @@ from rest_framework import generics
 class ReservationList(ListAPIView):
     serializer_class = ReservationSerializer
     permission_classes = [permissions.IsAuthenticated]
+
     def get_queryset(self):
         user = self.request.user
         customer = Customer.objects.get(name=user.username)
-        return Reservation.objects.filter(customer=customer)
+        if user.is_staff:
+            return Reservation.objects.all()
+        else:
+            return Reservation.objects.filter(customer=customer)
 
 class ReservationCreateView(CreateAPIView):
     queryset = Reservation.objects.all()
@@ -26,7 +30,10 @@ class ReservationDetailView(RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         customer = Customer.objects.get(name=user.username)
-        return Reservation.objects.filter(customer=customer)
+        if user.is_staff:
+            return Reservation.objects.all()
+        else:
+            return Reservation.objects.filter(customer=customer)
 
     def get_object(self):
         queryset = self.get_queryset()
