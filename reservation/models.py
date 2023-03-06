76287ad719f6ed6572ed0e338eecc28d0615a9c6
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-03-06 08:49:34 +0200

    Update models.py

--- reservation/models.py
+++ reservation/models.py
@@ -9,5 +9,14 @@ class Reservation(models.Model):
     start_date = models.DateField(auto_now_add=False, null=False)
     end_date = models.DateField(auto_now_add=False, null=False)
 
+    def save(self, *args, **kwargs):
+        self.car.available = False
+        self.car.save()
+        super(Reservation, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.car.available = True
+        self.car.save()
+        super(Reservation, self).delete(*args, **kwargs)
     def __str__(self):
         return str(self.car) + '-' + str(self.customer)
