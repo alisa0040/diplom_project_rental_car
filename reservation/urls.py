9e157c23501961bab7b4464d220fe90dfa4c9926
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-03-06 08:47:43 +0200

    Update urls.py

--- reservation/urls.py
+++ reservation/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('list/', ReservationList.as_view(), name='reservation'),
     path('create/', ReservationCreateView.as_view(), name='reservation-create'),
     path('list/<int:pk>/', ReservationDetailView.as_view(), name='reservation-detail'),
-    # path('list/<int:pk>/delete/', ReservationDeleteView.as_view(), name='reservation-delete'),
-    # path('list/<int:pk>/update/', ReservationUpdateView.as_view(), name='reservation-update'),
 ]
