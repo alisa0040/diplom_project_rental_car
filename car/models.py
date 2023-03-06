cebf0b9b97bd386ff74e7b9075b0c11486e8f3f7
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-03-06 09:04:38 +0200

    Update models.py

--- car/models.py
+++ car/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 from employer.models import Employer
 
 
@@ -33,7 +33,6 @@ class FuelType(models.Model):
         return self.name
 
 
-
 class CarOption(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(null=True)
@@ -41,30 +40,33 @@ class CarOption(models.Model):
     def __str__(self):
         return self.name
 
+
 class CarTransmission(models.Model):
     name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
 
+
 class Car(models.Model):
     brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE, null=True, related_name='cars')
     car_model = models.ForeignKey(CarModel, on_delete=models.CASCADE, null=True, related_name='cars')
     transmission = models.ForeignKey(CarTransmission, on_delete=models.CASCADE, null=True, related_name='cars')
     fueltype = models.ForeignKey(FuelType, on_delete=models.CASCADE, null=True, related_name='cars')
-    options = models.ManyToManyField(CarOption ,blank=True)
+    options = models.ManyToManyField(CarOption, blank=True)
     year = models.IntegerField(verbose_name='Year')
     color = models.CharField(max_length=255, verbose_name='Color')
-    cost_per_day = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='cost per day $',default=0.0,null=True)
-    is_available = models.BooleanField(default=True, verbose_name='Is Available')
+    cost_per_day = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='cost per day $', default=0.0,
+                                       null=True)
     is_employer = models.ForeignKey(Employer, on_delete=models.CASCADE, null=True)
+    available = models.BooleanField(default=True)
     objects = CarManager()
 
     def __str__(self):
         return str(self.brand) + '-' + str(self.car_model) + '-' + str(self.is_employer)
 
-
 class CarImage(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True,related_name='car_image')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, related_name='car_image')
     image = models.ImageField(upload_to='car_images/')
     caption = models.CharField(max_length=200, blank=True, null=True)
+
