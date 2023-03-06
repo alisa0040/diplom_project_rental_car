531fac3dab9b0065696406324ff594671208a4c5
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2023-03-06 08:53:43 +0200

    Update views.py

--- rental/views.py
+++ rental/views.py
@@ -1,34 +1,39 @@
 from datetime import datetime
 from django.core.exceptions import ValidationError
 from rest_framework import permissions
-from rest_framework.generics import UpdateAPIView, DestroyAPIView, ListAPIView, CreateAPIView
+from rest_framework.generics import UpdateAPIView, DestroyAPIView, ListAPIView, CreateAPIView,RetrieveUpdateDestroyAPIView
 from rental.models import Rental
+from customer.models import Customer
+from car.models import Car
 from rental.serializers import RentalSerializer
 from reservation.models import Reservation
-
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 class RentalList(ListAPIView):
-    queryset = Rental.objects.all()
     serializer_class = RentalSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        user = self.request.user
+        customer = Customer.objects.get(name=user.username)
+        return Rental.objects.filter(customer=customer)
 
 class RentalCreateView(CreateAPIView):
     queryset = Rental.objects.all()
     serializer_class = RentalSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-
-
-
-class RentalUpdateView(UpdateAPIView):
-    queryset = Rental.objects.all()
+class RentalDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = RentalSerializer
     permission_classes = [permissions.IsAuthenticated]
-    lookup_field = 'pk'
 
-class RentalDeleteView(DestroyAPIView):
-    queryset = Rental.objects.all()
-    serializer_class = RentalSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    lookup_field = 'pk'
+    def get_queryset(self):
+        user = self.request.user
+        customer = Customer.objects.get(name=user.username)
+        return Reservation.objects.filter(customer=customer)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, pk=self.kwargs["pk"])
+        return obj
